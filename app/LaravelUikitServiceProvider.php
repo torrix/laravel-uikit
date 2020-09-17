0a21df422ca63dc1a45ede5c54f313d8273d7f6d
Matt Fletcher <matt@torrix.uk>
2020-09-17 23:08:44 +0100

    typo

--- app/LaravelUikitServiceProvider.php
+++ app/LaravelUikitServiceProvider.php
@@ -17,17 +17,17 @@ class LaravelUikitServiceProvider extends ServiceProvider
             Fortify::verifyEmailView(fn() => view('laravel-uikit::auth.verify-email'));
         }
 
-        $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
 
-        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'laravel-uikit');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'laravel-uikit');
 
         $this->publishes(
             [
-                __DIR__ . '/../../webpack.mix.js'                    => 'webpack.mix.js',
-                __DIR__ . '/../../resources/js'                      => resource_path('js'),
-                __DIR__ . '/../../resources/sass'                    => resource_path('sass'),
-                __DIR__ . '/../../resources/views/welcome.blade.php' => resource_path('views/welcome.blade.php'),
-                __DIR__ . '/../../resources/views/layouts'           => resource_path('views/layouts'),
+                __DIR__ . '/../webpack.mix.js'                    => 'webpack.mix.js',
+                __DIR__ . '/../resources/js'                      => resource_path('js'),
+                __DIR__ . '/../resources/sass'                    => resource_path('sass'),
+                __DIR__ . '/../resources/views/welcome.blade.php' => resource_path('views/welcome.blade.php'),
+                __DIR__ . '/../resources/views/layouts'           => resource_path('views/layouts'),
             ]
         );
     }
